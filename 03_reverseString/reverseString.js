84f237bb549df4c486b36d4bea8cb51330a7dec7
dustein <steindu@gmail.com>
2022-06-14 13:46:01 -0300

    exerciose 02 passed tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(palavras) {
+   
+   palavras.join(" ")
+   console.log(palavras)
+
+   return null;
 
 };
 
+reverseString("teste")
 // Do not edit below this line
 module.exports = reverseString;
