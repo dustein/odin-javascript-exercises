8052c5caa40506b5840eea6b21cfcd0f3d84d182
dustein <steindu@gmail.com>
2022-06-14 12:44:40 -0300

    exercise 02 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(palavra, vezes) {
+   let resp = [];
+   
+   for (i=1; i <= vezes; i++) {
+      resp.push(palavra);
+   }
+   console.log(resp.join(""))
+   return resp.join("")
 };
 
+repeatString('hey', 10);
 // Do not edit below this line
 module.exports = repeatString;
