7f29380e24a7a1a9834f4e7dd5100321bd71be11
dustein <steindu@gmail.com>
2022-06-15 16:00:26 -0300

    exe 04 passou 4 testes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,39 @@
 const removeFromArray = function(array, ...toremove) {
 
+   console.log("array original : " + array)
    let removeMany = toremove.length
-   console.log("toremoveLength : " + toremove.length)
-   
-   //remove multiples values
-   for (i=0; i<removeMany; i++) {
-      let removeIndex = array.indexOf(toremove[i]);
-
-      //ignores non presents values but work if tehe is on
-      for(i-0; i<removeMany; i++) {
-         //ignore non present values
-         if(removeIndex === -1) {
-            console.log("nada encontrado... " + array)
-            return array
-         }
+   // console.log("toremoveLength : " + toremove.length)
+   let removeArray = [...toremove]
+   console.log("removeArray : " + removeArray)
+   //para cada toremove
+   for(i=0; i<removeArray.length; i++) {
+      console.log(removeArray.length)
+      console.log("removeArray[i] : " + removeArray[i])
+      for (j=0; j<removeMany; j++) {
+         let removeIndex = array.indexOf(removeArray[i]);
          console.log("removeIndex : " + removeIndex)
-         array.splice(removeIndex, 1)
-         console.log("array : " + array)
+         if(removeIndex != -1) {
+            array.splice(removeIndex, 1)
+            console.log("array : " + array)
+         }
       }
    }
+
+
+
+   // //remove multiples values
+   // for (i=0; i<removeMany; i++) {
+   //    let removeIndex = array.indexOf(toremove[i]);
+   //    //ignore non present values
+   //    // if(removeIndex === -1) {
+   //    //    removeArray.shift();
+   //    //    console.log("removeArray shifted : " + removeArray)
+   //    // }
+      
+   //          console.log("removeIndex : " + removeIndex)
+   //          array.splice(removeIndex, 1)
+   //          console.log("array : " + array)
+   // }
 
    console.log(array)
    return array
