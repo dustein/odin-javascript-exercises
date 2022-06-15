753e45495203f137510818d032ea7ea516aa099a
dustein <steindu@gmail.com>
2022-06-15 14:42:48 -0300

    exerc 04 passou 3 testes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toremove) {
 
+   let removeMany = toremove.length
+   console.log("toremoveLength : " + toremove.length)
+   
+   //remove multiples values
+   for (i=0; i<removeMany; i++) {
+      let removeIndex = array.indexOf(toremove[i]);
+
+      //ignores non presents values but work if tehe is on
+      for(i-0; i<removeMany; i++) {
+         //ignore non present values
+         if(removeIndex === -1) {
+            console.log("nada encontrado... " + array)
+            return array
+         }
+         console.log("removeIndex : " + removeIndex)
+         array.splice(removeIndex, 1)
+         console.log("array : " + array)
+      }
+   }
+
+   console.log(array)
+   return array
 };
 
+removeFromArray([1,2,3,4], 7, 2)
 // Do not edit below this line
 module.exports = removeFromArray;
