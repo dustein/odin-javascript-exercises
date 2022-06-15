bb655f6c10d3f655d3f6cace985f89db217d6b3c
dustein <steindu@gmail.com>
2022-06-15 16:25:49 -0300

    ex 05 passou 2 testes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
 
+   let soma = 0;
+
+   for(i=1; i<=n2; i++) {
+      console.log("soma: " + soma, "i: "  + i)
+      soma+= i   
+   }
+   console.log("soma: " + soma)
+
+   return soma;
 };
 
+sumAll(1, 4000)
 // Do not edit below this line
 module.exports = sumAll;
