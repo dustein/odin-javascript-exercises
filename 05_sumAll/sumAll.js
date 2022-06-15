5015ea95f762454488210504902880803d312f3d
dustein <steindu@gmail.com>
2022-06-15 16:32:43 -0300

    ex 05 passou 4 testes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,11 +4,15 @@ const sumAll = function(n1, n2) {
    let a = n1;
    let b = n2;
 
+   if (a < 0 || b < 0) {
+      console.log("ERROR")
+      return 'ERROR'
+   }
    if (n2 < n1) {
       a = n2
       b = n1
    }
-   
+
    for(i=1; i<=b; i++) {
       console.log("soma: " + soma, "i: "  + i)
       soma+= i   
@@ -18,6 +22,6 @@ const sumAll = function(n1, n2) {
    return soma;
 };
 
-sumAll(123, 1)
+sumAll(-10, 4)
 // Do not edit below this line
 module.exports = sumAll;
