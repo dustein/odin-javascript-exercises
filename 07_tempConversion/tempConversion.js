5f01a14f729be8da078a46613d719213542d0b2d
DuStein <steindu@gmail.com>
2022-06-17 16:46:17 -0300

    desenvolvendo exe 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsius = Math.floor(((temp - 32) * 5)/9)
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenheit = Math.floor((temp * 9/5) + 32)
+  return fahrenheit;
 };
-
+console.log(ftoc(32))
+console.log(ctof(26))
 // Do not edit below this line
 module.exports = {
   ftoc,
