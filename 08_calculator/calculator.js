ed8467de93b97ba52b3aa899de4e61c546b5e1c2
DuStein <steindu@gmail.com>
2022-06-18 19:44:44 -0300

    passed 8 tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,18 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let acumulador = 0;
+  
+  for(i=0; i<arr.length; i++) {
+    acumulador += arr[i]
+  }
+  return acumulador;
 };
 
 const multiply = function() {
@@ -22,6 +27,9 @@ const factorial = function() {
 	
 };
 
+// console.log(add(0, 0))
+// console.log(subtract(10, 4))
+console.log(sum([]))
 // Do not edit below this line
 module.exports = {
   add,
