6198f1c3e3bd33dc6e51374e9033c41035aadc75
DuStein <steindu@gmail.com>
2022-06-18 22:29:21 -0300

    11 tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -19,16 +19,14 @@ const multiply = function(arr) {
 	let acumulador = 1;
 
   for(i=0; i<arr.length; i++) {
-    console.log(arr)
-    console.log(arr.length)
-    console.log(arr[i])
     acumulador *= arr[i]
   }
   return acumulador;
 };
 
-const power = function() {
-	
+const power = function(base, expoente) {
+
+  return base ** expoente
 };
 
 const factorial = function() {
@@ -38,7 +36,8 @@ const factorial = function() {
 // console.log(add(0, 0))
 // console.log(subtract(10, 4))
 // console.log(sum([]))
-console.log(multiply([2,4]))
+// console.log(multiply([2,4]))
+console.log(power(4,3))
 // Do not edit below this line
 module.exports = {
   add,
