b7200525d2da5813ce39605dae92c50f00a11308
DuStein <steindu@gmail.com>
2022-06-19 21:22:44 -0300

    all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -29,15 +29,27 @@ const power = function(base, expoente) {
   return base ** expoente
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+  if(num === 0) {
+    return 1
+  }
+  return factorial(num - 1) * num
+
+
+	// fatorial 0 = 1
+  // fatorial 1 = 1*1
+  // fatorial 2 = 2*1
+  // fatorial 3 = 3*2*1
+  // fatorial 4 = 4*3*2*1
 };
 
 // console.log(add(0, 0))
 // console.log(subtract(10, 4))
 // console.log(sum([]))
 // console.log(multiply([2,4]))
-console.log(power(4,3))
+// console.log(power(4,3))
+console.log(factorial(10))
 // Do not edit below this line
 module.exports = {
   add,
