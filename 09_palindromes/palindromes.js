92394a1887d004a5604c1ef086fb4808da6e34fc
dustein <steindu@gmail.com>
2022-06-20 11:12:43 -0300

    refatorando

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,19 +1,28 @@
 const palindromes = function (info) {
-
-  let fase1 = info.toLowerCase();
-  console.log(fase1)
-  let fase2 = fase1.split("");
-  console.log(fase2)
-  let fase3 = fase2.reverse();
-  console.log(fase3)
-  console.log(fase3.lenght)
-  for(i=0; i<6; i++) {
-    console.log(fase3[i])
+  let reverseArray = []
+  let prepArray = info
+                      .toLowerCase()
+                      .split("")
+                      .reverse();
+  // let fase1 = info.toLowerCase();
+  // console.log(fase1)
+  // let fase2 = fase1.split("");
+  // console.log(fase2)
+  // let fase3 = fase2.reverse();
+  // console.log(fase3)
+  // console.log(fase3.length)
+  for(i=0; i<prepArray.length; i++) {
+    reverseArray.push(prepArray[i])
   }
-
+  let teste = reverseArray.join("")
+  console.log(reverseArray)
+  console.log(teste)
+  if(teste === info) {
+    return "OK"
+  }
   return "---- rodou -----"
 };
 
-console.log(palindromes("TesSte"))
+console.log(palindromes("racecar"))
 // Do not edit below this line
 module.exports = palindromes;
