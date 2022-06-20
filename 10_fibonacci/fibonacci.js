2944409b7e615492e0c44b26954a30949c6475f5
dustein <steindu@gmail.com>
2022-06-20 16:53:06 -0300

    iniciando fibro

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+   if(position < 0) {
+      return "OOPS"
+   }
+   let posNum = Number(position);
+
+   
 
 };
 
+console.log(fibonacci("8"))
 // Do not edit below this line
 module.exports = fibonacci;
