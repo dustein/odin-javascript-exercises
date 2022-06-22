b5d4c0608f36169a772ccbb0d2aa2daf4d66cb48
DuStein <steindu@gmail.com>
2022-06-22 13:12:17 -0300

    test passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+  const titulos = [];
+
+  for(let i=0; i<books.length; i++) {
+    titulos.push(books[i].title)
+  }
+  return titulos
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+console.log(getTheTitles(books))
 
 // Do not edit below this line
 module.exports = getTheTitles;
