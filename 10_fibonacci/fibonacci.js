5be80d9d57f143f0978e7c5d5bd35c11c3174285
DuStein <steindu@gmail.com>
2022-06-22 09:38:58 -0300

    all 9 testes passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,11 +1,17 @@
 const fibonacci = function(position) {
-   if(position < 0) {
-      return "OOPS"
-   }
+   let sequence = [0, 1];
    let posNum = Number(position);
 
-   
+   if(posNum < 0) {
+      return "OOPS"
+   };
 
+   for(i=2; i<posNum+1; i++) {
+      sequence[i] = sequence[i-2] + sequence[i-1];
+      console.log(sequence)
+   }
+
+   return sequence[posNum]
 };
 
 console.log(fibonacci("8"))
