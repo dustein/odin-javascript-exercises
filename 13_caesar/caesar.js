38f950c9e46076ba68cc9540cffd98c465c9da05
dustein <steindu@gmail.com>
2022-06-24 16:39:51 -0300

    desenvolvendo solucao teste 3

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,13 +1,33 @@
 const caesar = function(original, change) {
    let changedLetters = []
-
+   let regEx = /[aA-zZ]/g
+   let codigo = 0;
    for(let i = 0 ; i < original.length; i++) {
-      changedLetters.push(String.fromCharCode(original.charCodeAt(i)+change))
-      console.log(changedLetters)
+
+      if(original[i].match(regEx)) {
+         // console.log(original[i])
+         
+         if(original.charCodeAt(i) > 64 && original.charCodeAt(i) < 91) {
+            if(original.charCodeAt(i) + change > 90 ) {
+               codigo = 64 + (change - (90 - original.charCodeAt(i)))
+               console.log(codigo)
+               changedLetters.push(String.fromCharCode(original.charCodeAt(codigo)))      
+         }
+
+         }
+
+
+
+
+         changedLetters.push(String.fromCharCode(original.charCodeAt(i)+change))
+      } else if (!original[i].match(regEx)) {
+         changedLetters.push(String.fromCharCode(original.charCodeAt(i)))
+      }
+      // console.log(changedLetters)
    }
    return changedLetters.join("");
 };
 
-console.log(caesar("Aaa", 1))
+console.log(caesar("Hello, World!", 5))
 // Do not edit below this line
 module.exports = caesar;
