eba2fb409e04373869e0bc537296d87ad8808eae
dustein <steindu@gmail.com>
2022-06-24 15:31:35 -0300

    1 test passed

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,15 @@
-const caesar = function() {
+const caesar = function(original, change) {
+   let changedLetters = []
 
+   for(let i = 0 ; i < original.length; i++) {
+      changedLetters.push(original.charCodeAt(i))
+      console.log(changedLetters)
+   }
+   
+   // let originalCaractere = original.charCodeAt(0);
+   // return String.fromCharCode(originalCaractere + change)
 };
 
+console.log(caesar("Aaa", 1))
 // Do not edit below this line
 module.exports = caesar;
