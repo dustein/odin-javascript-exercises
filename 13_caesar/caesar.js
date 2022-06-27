19480b511b95c266d6cd9fe70a19cdf366d45c7f
dustein <steindu@gmail.com>
2022-06-27 13:01:55 -0300

    alterado codigo

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,6 @@
 const caesar = function(original, change) {
 
-   let changedLetters = []
+   let changedLetters = "";
    let regex = /[aA-zZ]/g;
 
 
@@ -8,28 +8,29 @@ const caesar = function(original, change) {
       let ascii = original[i].charCodeAt();
       console.log(`Letra da vez: ${original[i]} codigo ${ascii}`)
       //verificar se e letra
-      if(!original[i].match(regex)) {
-         console.log(original[i])
-      }
+      // if(!original[i].match(regex)) {
+      //    console.log(original[i])
+      // }
 
       if(ascii >= 65 && ascii <= 90) {
          console.log("MAIUSCULA")
-
-      }
-
-      if(ascii >= 97 && ascii <= 122) {
+         changedLetters += String.fromCharCode( (ascii - "A".charCodeAt(0) + change) % 26 + "A".charCodeAt(0));
+      } else if(ascii >= 97 && ascii <= 122) {
          console.log("minuscula")
-         
+         changedLetters += String.fromCharCode( (ascii - "a".charCodeAt(0) + change) % 26 + "a".charCodeAt(0));
+      } else {
+         changedLetters += original[i]
       }
 
 
    }
 
 
-   return changedLetters.join("");
+   // return changedLetters.join("");
+   return changedLetters;
 };
 
-console.log(caesar("Hello, World!", 1))
+console.log(caesar("Hello, World!", 5))
    // const maiusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
    // const minusculas = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Do not edit below this line
